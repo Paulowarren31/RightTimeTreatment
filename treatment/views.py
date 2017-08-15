76cb7f13f92c601d788e3cbe34218866fea785c4
paulowar <paulowar@umich.edu>
2017-08-15 13:56:38 -0400

    adding stuf

--- treatment/views.py
+++ treatment/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+def index(request, user_id):
+  return render(request, 'base.html')
+
